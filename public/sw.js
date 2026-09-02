402d806a49394e25d53a547285cd0dfc3687b591
Smarue95 <tiago9512@gmail.com>
2026-09-01 23:27:17 -0500

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,12 @@
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
 
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    fetch(event.request).catch(() => new Response('', { status: 503, statusText: 'offline' }))
+  );
+});
+
 self.addEventListener('push', function (event) {
   let data = {};
   try {
